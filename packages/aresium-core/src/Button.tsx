c89f416f2beb0fd6f735fe4626c125a97376eb3d
Andrei Dorobantu <elfandreis@gmail.com>
2022-06-20 00:49:50 +0300

    Feature: Changed storybook to use tsx files

--- packages/aresium-core/src/Button.tsx
+++ packages/aresium-core/src/Button.tsx
@@ -1,4 +1,5 @@
 import { styled } from "../stitches.config";
+
 export const Button = styled("button", {
   all: "unset",
   alignItems: "center",
@@ -15,17 +16,17 @@ export const Button = styled("button", {
   variants: {
     size: {
       "1": {
-        borderRadius: "$3",
-        height: "$4",
+        borderRadius: "$sm",
+        height: "$1",
         px: "$2",
-        fontSize: "$2",
+        fontSize: "$sm",
         boxShadow: "$1",
       },
       "2": {
-        borderRadius: "$4",
+        borderRadius: "$md",
         height: "$5",
         px: "$3",
-        fontSize: "$3",
+        fontSize: "$md",
         boxShadow: "$1",
       },
     },
