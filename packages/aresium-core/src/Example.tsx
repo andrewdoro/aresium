688ec93e82e493cc9c372cfe41a893f6a0923bc9
Andrei Dorobantu <elfandreis@gmail.com>
2022-07-05 00:38:56 +0300

    Feature: added pnpm (#6)

---
+++ packages/aresium-core/src/Example.tsx
@@ -0,0 +1,32 @@
+import type * as Stitches from "@stitches/react";
+import { styled, modifyVariantsForStory } from "../stitches.config";
+
+const Example = styled("div", {
+  variants: {
+    size: {
+      ["13213p"]: {
+        borderRadius: "$sm",
+        height: "$1",
+        px: "$2",
+        fontSize: "$sm",
+        boxShadow: "$1",
+      },
+      ["232p"]: {
+        borderRadius: "$md",
+        height: "$5",
+        px: "$3",
+        fontSize: "$md",
+        boxShadow: "$1",
+      },
+    },
+  },
+});
+
+export default Example;
+
+type ExampleVariants = Stitches.VariantProps<typeof Example>;
+interface ExampleProps extends ExampleVariants {}
+// Use this as the type in Story; i.e. `ComponentMeta<typeof ButtonStory>`
+export const ExampleStory = modifyVariantsForStory<ExampleVariants, ExampleProps, typeof Example>(
+  Example
+);
--- packages/aresium-core/src/index.tsx
+++ packages/aresium-core/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-export { Button } from "./Button";
+export { Button, ButtonForStory } from "./Button";
 export { Paper } from "./Paper";
 export { Box } from "./Box";
 export { DialogDemo } from "./Dialog";
-export { Flex } from "./Flex";
+export { Flex, FlexStory } from "./Flex";
+export { ExampleStory } from "./Example";
